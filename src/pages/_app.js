ea9f4547943008a376370960d308269416ed11f1
Sahil kumar <sahil.kumar@redbasil.in>
2023-05-20 23:58:46 +0530

    updated with product js file

---
+++ src/pages/_app.js
@@ -0,0 +1,10 @@
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+import "primereact/resources/primereact.min.css";                                       
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+  }
+  export default MyApp;
