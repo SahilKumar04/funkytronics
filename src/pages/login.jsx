f302593696df8d5a4226c9f4d713cf69754bc310
ritesh.k3 <ritesh.k@redbasil.in>
2023-06-02 15:04:59 +0530

    added user slice

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -5,21 +5,31 @@ import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import axios from 'axios';
 import { useRouter } from 'next/router';
+import { useDispatch } from 'react-redux';
+import { setAddress, setEmail, setGender, setName, setUserId, setAge } from '@/store/userSlice';
 
 const Login = () => {
   const [user, setUser] = useAuthState(auth);
   const googleAuth = new GoogleAuthProvider();
+  const dispatch = useDispatch();
   const router = useRouter();
   const login = async () => {
     const result = await signInWithPopup(auth, googleAuth);
 
-    const user = await axios.get(
+    let user = await axios.get(
       `/api/getUserByUserId?userId=${result.user.uid}`
     );
 
-    if (!user.data) {
+    user = user.data;
+    if (!user) {
       router.push('/create_user');
     } else {
+      dispatch(setName(user.name))
+      dispatch(setEmail(user.email))
+      dispatch(setGender(user.gender))
+      dispatch(setAge(user.age))
+      dispatch(setUserId(result.user.uid))
+      dispatch(setAddress(user.address))
       router.push('/');
     }
   };
---
+++ src/store/userSlice.js
@@ -0,0 +1,55 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// Initial state
+const initialState = {
+    name: '',
+    email: '',
+    gender: '',
+    age: 18,
+    address: [],
+    currentAddress: '',
+    userId: ''
+};
+
+// Actual Slice
+export const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        setName(state, action) {
+          state.name = action.payload;
+        },
+    
+        setEmail(state, action) {
+          state.email = action.payload;
+        },
+    
+        setGender(state, action) {
+          state.gender = action.payload;
+        },
+    
+        setAge(state, action) {
+          state.age = action.payload;
+        },
+    
+        setAddress(state, action) {
+          state.address = [...state.address, action.payload];
+        },
+
+        setCurrentAddress(state, action) {
+            state.currentAddress = action.payload;
+        },
+
+        setUserId(state, action) {
+            state.userId = action.payload;
+        },
+    
+        reset() {
+          return initialState;
+        },
+      }
+});
+
+export const { setAddress, setName, setAge, setEmail, setGender, setCurrentAddress, setUserId } = userSlice.actions;
+
+export default userSlice.reducer;
